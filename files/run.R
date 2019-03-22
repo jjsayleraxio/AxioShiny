85ee776fa0d08b0f5fe30ceb09c75618c5ced2ba
Joseph Sayler <josephs@axioresearch.com>
2019-03-22 13:06:21 -0700

    Update run.R
    shortened base URL for plumber.R file - user to supply rest of path in PLUMBER variable

--- files/run.R
+++ files/run.R
@@ -1,3 +1,3 @@
 library(plumber)
-pr<-plumber::plumb(paste0("/srv/shiny-server/app/",Sys.getenv("PLUMBER")))
+pr<-plumber::plumb(paste0("/srv/shiny-server/",Sys.getenv("PLUMBER")))
 pr$run(host='0.0.0.0',port=40000)
