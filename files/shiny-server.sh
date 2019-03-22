2d4f7bb20b116926241c0d3fd61b20cf8af873db
Joseph Sayler <josephs@axioresearch.com>
2019-03-22 09:55:36 -0700

    Update shiny-server.sh
    added code to create a .Renviron file at start up that takes environment variables APP_SERVER and PLUMBER and place them into this file for use by the app

--- files/shiny-server.sh
+++ files/shiny-server.sh
@@ -4,6 +4,13 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
+# this takes environment variables supplied by user at run time
+# and places them in a .Renviron file so that the shiny app
+# can use them to determine file and server locations
+echo "APP_SERVER=$APP_SERVER" > /home/shiny/.Renviron
+echo "PLUMBER=$PLUMBER" >> /home/shiny/.Renviron
+chown 999:999 /home/shiny/.Renviron
+
 Rscript run.R &
 
 if [ "$APPLICATION_LOGS_TO_STDOUT" = "false" ];
